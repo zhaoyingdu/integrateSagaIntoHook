10b0bd112c5c917027eeb68f0ec9ebb9ed249070
Zhaoying Du <42394377+zhaoyingdu@users.noreply.github.com>
2019-04-25 03:23:20 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import Counter from "./Itr1.js";
 import CounterSum from "./Itr2.js";
 import Iter3 from "./Itr3.js";
-import Iter5 from "./itr4";
+import Iter5 from "./Iter5.js";
 export default () => (
   <div>
     <Iter5 />
